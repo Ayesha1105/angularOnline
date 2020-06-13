326d42cbd6ae4fa3e4bbd90daf6711040419cb8e
Ayesha1105 <bagwanayesha72@gmail.com>
2020-06-13 22:02:44 +0530

    commit Layout

--- ngOnline/src/app/app.module.ts
+++ ngOnline/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SqrtPipe } from './pipes/sqrt.pipe';
+import { BannerTwoComponent } from './layout/banner-two/banner-two.component';
+import { BannerThreeComponent } from './layout/banner-three/banner-three.component';
+import { ContactUsComponent } from './layout/contact-us/contact-us.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ProductsComponent } from './layout/products/products.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { BannerOneComponent } from './layout/banner-one/banner-one.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SqrtPipe, BannerTwoComponent, BannerThreeComponent, ContactUsComponent, FooterComponent, ProductsComponent, NavComponent, BannerOneComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
